c2c95e8a41592b9a82b4094cc8c3b1497f6601bf
HaraldIsHere <duplouis.harald@gmail.com>
2025-05-28 11:59:13 +0200

    init

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const displayedGIFS = document.querySelector(`.data`);
+const generateButton = document.querySelector(`.generate-gif`)
+
+// Inputs
+const searchInput = document.querySelector(`.search-input`);
+const numberInput = document.querySelector(`.number-input`);
+
+async function getGif() {
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(searchInput.value)}&api_key=GEmjEES5bxqnnyJA6fETMXMRRlq8ssOX&limit=${numberInput.value}`);
+    const data = await response.json();
+    console.log(data);
+    
+    return data.data.map(gif => gif.images.fixed_height.url); 
+  } catch (error) {
+    console.error("Erreur :", error);
+    return [];
+  }
+}
+
+// getGif()
+
+async function displayGIF() {
+  const imagesList = await getGif();
+    displayedGIFS.innerHTML = ``;
+
+    imagesList.forEach(element => {
+        displayedGIFS.innerHTML += `<div class="data-element"><img src="${element}" alt="Random Fox"></div>`;
+    });
+}
+
+
+generateButton.addEventListener(`click`, (e) => {
+  e.preventDefault();
+  displayGIF();
+});
